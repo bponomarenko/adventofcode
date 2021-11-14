ef8756696f1b18a799a71e6317cb4fd0f5e07371
Borys Ponomarenko <bponomarenko@gmail.com>
2021-11-14 20:34:13 +0100

    Make sure we can read multiline test cases

--- bin/index.js
+++ bin/index.js
@@ -3,6 +3,7 @@
 const { program } = require('commander');
 const chalk = require('chalk');
 const nodemon = require('nodemon');
+const readline = require('readline');
 require('dotenv').config();
 
 const withDayAndYear = command => command
@@ -27,6 +28,21 @@ const runCommand = async (name, args, watch) => {
   }
 };
 
+const readInput = async () => new Promise(resolve => {
+  const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+  const lines = [];
+  rl.write('Enter test input data:\n');
+  rl.prompt();
+  rl.on('line', line => {
+    if (line.trim()) {
+      lines.push(line.trim());
+    } else {
+      rl.close();
+    }
+  });
+  rl.on('close', () => resolve(lines.join('\n')));
+});
+
 withDayAndYear(program.command('init'))
   .description('Scaffolds necessary files for a specific puzzle of the day and year')
   .action(({ year, day }) => runCommand('init', [year, day]));
@@ -40,9 +56,12 @@ withDayAndYear(program.command('solve'))
 
 withDayAndYear(program.command('add-test'))
   .argument('<part>', 'Defines which part of the solution to run – part 1 or part 2', Number)
-  .argument('<input>', 'Test input')
   .argument('<answer>', 'Expected answer')
-  .action((part, input, answer, { year, day }) => runCommand('add-test', [year, day, part, input, answer]));
+  .argument('[input]', 'Test input')
+  .action(async (part, answer, inputArg, { year, day }) => {
+    const input = inputArg || await readInput();
+    return runCommand('add-test', [year, day, part, input, answer]);
+  });
 
 withDayAndYear(program.command('validate'))
   .argument('<part>', 'Defines which part of the solution to run – part 1 or part 2', Number)
