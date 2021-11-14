5be45dd4d0906d8ab792ebebe237379bf1d5d794
Borys Ponomarenko <bponomarenko@gmail.com>
2021-11-14 19:42:18 +0100

    Add add set of comments which would help to solve puzzles

---
+++ README.md
@@ -0,0 +1,8 @@
+Small project to automate some manual work for "Advent of code" puzzles
+
+* `npx aoc init` scaffolds files with code for solution and puzzle input for today'z puzzle
+* `npx aoc solve` executes the solution and can optionally submit the answer
+* `npx aoc add-test` adds test case to validate solution against
+* `npx aoc validate` validates current solution against all configured test cases
+
+Project requires `.env` file created with the session cookie: `SESSION_COOKIE=....`
---
+++ bin/index.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+
+const { program } = require('commander');
+const chalk = require('chalk');
+const nodemon = require('nodemon');
+require('dotenv').config();
+
+const withDayAndYear = command => command
+  .option('-y, --year <year>', 'Year of the puzzle to scaffold', Number, process.env.YEAR || new Date().getFullYear())
+  .option('-d, --day <day>', 'Day of the puzzle to scaffold', Number, new Date().getDate());
+
+const runCommand = async (name, args, watch) => {
+  if (watch) {
+    await new Promise(resolve => {
+      const argStr = program.args.filter(arg => arg !== '-w' && arg !== '--watch').join(' ');
+      nodemon({ exec: `bin/index.js ${argStr}`, ext: 'js,json' })
+        .on('start', () => console.log(chalk.gray('Restarting')))
+        .on('quit', () => resolve())
+        .on('exit', () => resolve());
+
+      // eslint-disable-next-line no-process-exit -- make sure nodemon is stopped on the first Ctrl+C
+      process.on('SIGINT', () => process.exit());
+    });
+  } else {
+    const command = require(`../lib/${name}`);
+    await command(...args);
+  }
+};
+
+withDayAndYear(program.command('init'))
+  .description('Scaffolds necessary files for a specific puzzle of the day and year')
+  .action(({ year, day }) => runCommand('init', [year, day]));
+
+withDayAndYear(program.command('solve'))
+  .argument('<part>', 'Defines which part of the solution to run – part 1 or part 2', Number)
+  .description('Runs puzzle solution code with specified input and prints the answer')
+  .option('-s, --submit', 'Would try to submit found answer')
+  .option('-w, --watch', 'Runs solution in a watch mode. Helpful for development')
+  .action((part, args) => runCommand('solve', [args.year, args.day, part, args.submit], args.watch));
+
+withDayAndYear(program.command('add-test'))
+  .argument('<part>', 'Defines which part of the solution to run – part 1 or part 2', Number)
+  .argument('<input>', 'Test input')
+  .argument('<answer>', 'Expected answer')
+  .action((part, input, answer, { year, day }) => runCommand('add-test', [year, day, part, input, answer]));
+
+withDayAndYear(program.command('validate'))
+  .argument('<part>', 'Defines which part of the solution to run – part 1 or part 2', Number)
+  .option('-w, --watch', 'Runs solution in a watch mode. Helpful for development')
+  .option('-a, --all', 'Runs test cases for both parts but with one solution')
+  .action((part, {
+    year, day, watch, all,
+  }) => runCommand('validate', [year, day, part, all], watch));
+
+(async function main() {
+  try {
+    await program.parseAsync(process.argv);
+  } catch (error) {
+    console.error(chalk`{red {inverse Error} ${error.message}}`);
+    process.exitCode = 1;
+  }
+}());
