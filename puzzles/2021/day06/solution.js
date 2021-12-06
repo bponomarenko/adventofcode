7a77a9bf70cbf4389366b6260cbe988452f5e5a7
Borys Ponomarenko <bponomarenko@gmail.com>
2021-12-06 07:26:49 +0100

    Fishy day 6

---
+++ puzzles/2021/day06/solution.js
@@ -0,0 +1,26 @@
+export const formatInput = input => input.split(',').map(Number);
+
+const countFish = (input, steps) => {
+  let groups = new Map();
+  input.forEach(num => {
+    groups.set(num, (groups.get(num) || 0) + 1);
+  });
+
+  for (let i = 0; i < steps; i += 1) {
+    const changes = new Map();
+    for (let [num, count] of groups) {
+      if (num === 0) {
+        changes.set(6, (changes.get(6) || 0) + count);
+        changes.set(8, count);
+      } else {
+        changes.set(num - 1, (changes.get(num - 1) || 0) + count);
+      }
+    }
+    groups = changes;
+  }
+  return Array.from(groups.values()).reduce((acc, count) => acc + count, 0);
+};
+
+export const part1 = input => countFish(input, 80);
+
+export const part2 = input => countFish(input, 256);
