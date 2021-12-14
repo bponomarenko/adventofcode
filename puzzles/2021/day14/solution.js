2f3051eefa42b7b4c7f96520c693296f71e10f9d
Borys Ponomarenko <bponomarenko@gmail.com>
2021-12-14 08:54:39 +0100

    Quite a challenge this time

---
+++ puzzles/2021/day14/solution.js
@@ -0,0 +1,39 @@
+export const formatInput = input => {
+  const [template, rules] = input.split('\n\n');
+  return [template, new Map(rules.split('\n').map(rule => {
+    const [pair, insert] = rule.split(' -> ');
+    return [pair, [`${pair[0]}${insert}`, `${insert}${pair[1]}`]];
+  }))];
+};
+
+const findPolymerizationResult = ([template, rules], steps) => {
+  let pairCounts = new Map();
+  template
+    .split('')
+    .map((char, i) => `${char}${template[i + 1]}`)
+    .slice(0, -1)
+    .forEach(pair => {
+      pairCounts.set(pair, (pairCounts.get(pair) || 0) + 1);
+    });
+
+  // Apply polymerization rules
+  for (let i = 0; i < steps; i += 1) {
+    const nextCounts = new Map();
+    Array.from(pairCounts.entries()).forEach(([pair, count]) => {
+      rules.get(pair).forEach(nextPair => {
+        nextCounts.set(nextPair, (nextCounts.get(nextPair) || 0) + count);
+      });
+    });
+    pairCounts = nextCounts;
+  }
+
+  const counts = new Map([[template.at(-1), 1]]);
+  Array.from(pairCounts.entries()).forEach(([[char], count]) => {
+    counts.set(char, (counts.get(char) || 0) + count);
+  });
+  return Math.max(...counts.values()) - Math.min(...counts.values());
+};
+
+export const part1 = input => findPolymerizationResult(input, 10);
+
+export const part2 = input => findPolymerizationResult(input, 40);
