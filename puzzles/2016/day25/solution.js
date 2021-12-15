0d8274a9b2b40bdc7f4681ba644c5f5e432f284b
Borys Ponomarenko <bponomarenko@gmail.com>
2021-12-15 23:42:53 +0100

    Improve computer prototype so it would be faster

--- puzzles/2016/day25/solution.js
+++ puzzles/2016/day25/solution.js
@@ -1,37 +1,41 @@
-export const formatInput = input => input.split('\n');
-
 const isNumeric = value => !Number.isNaN(+value);
+const isNumber = value => typeof value === 'number';
+
+export const formatInput = input => input.split('\n').map(line => {
+  const [cmd, arg1, arg2] = line.split(' ');
+  return [cmd, isNumeric(arg1) ? +arg1 : arg1, arg2 && isNumeric(arg2) ? +arg2 : arg2];
+});
 
 const findAnswer = (input, initialValue, limit) => {
   const registers = { a: initialValue, b: 0, c: 0, d: 0 };
   let pointer = 0;
   let expected = 0;
 
-  const getValue = pos => (isNumeric(pos) ? +pos : registers[pos]);
+  const getValue = pos => (isNumber(pos) ? pos : registers[pos]);
 
   while (pointer >= 0 && pointer < input.length && limit) {
-    const [cmd, arg1, arg2] = input[pointer].split(' ');
+    const [cmd, arg1, arg2] = input[pointer];
     let tglIndex;
     let tglCmd;
 
     switch (cmd) {
       case 'cpy':
         pointer += 1;
-        if (isNumeric(arg2)) {
+        if (isNumber(arg2)) {
           break;
         }
         registers[arg2] = getValue(arg1);
         break;
       case 'inc':
         pointer += 1;
-        if (isNumeric(arg1)) {
+        if (isNumber(arg1)) {
           break;
         }
         registers[arg1] += 1;
         break;
       case 'dec':
         pointer += 1;
-        if (isNumeric(arg1)) {
+        if (isNumber(arg1)) {
           break;
         }
         registers[arg1] -= 1;
@@ -46,16 +50,16 @@ const findAnswer = (input, initialValue, limit) => {
         if (tglIndex < 0 || tglIndex >= input.length) {
           break;
         }
-        tglCmd = input[tglIndex].slice(0, 3);
+        [tglCmd] = input[tglIndex];
 
-        if (tglCmd.startsWith('inc')) {
-          input[tglIndex] = input[tglIndex].replace(tglCmd, 'dec');
-        } else if (tglCmd.startsWith('dec') || tglCmd.startsWith('tgl') || tglCmd.startsWith('out')) {
-          input[tglIndex] = input[tglIndex].replace(tglCmd, 'inc');
-        } else if (tglCmd.startsWith('jnz')) {
-          input[tglIndex] = input[tglIndex].replace(tglCmd, 'cpy');
-        } else if (tglCmd.startsWith('cpy')) {
-          input[tglIndex] = input[tglIndex].replace(tglCmd, 'jnz');
+        if (tglCmd === 'inc') {
+          input[tglIndex][0] = 'dec';
+        } else if (tglCmd === 'dec' || tglCmd === 'tgl' || tglCmd === 'out') {
+          input[tglIndex][0] = 'inc';
+        } else if (tglCmd === 'jnz') {
+          input[tglIndex][0] = 'cpy';
+        } else if (tglCmd === 'cpy') {
+          input[tglIndex][0] = 'jnz';
         }
         break;
       case 'out':
