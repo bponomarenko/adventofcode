a3a597a417a5771ea81e05ddd20ae56de0dcb482
Borys Ponomarenko <bponomarenko@gmail.com>
2022-12-08 23:28:43 +0100

    "fresh look" one year after gave me solution for this one

--- puzzles/2017/day17/solution.js
+++ puzzles/2017/day17/solution.js
@@ -1,24 +1,29 @@
 export const formatInput = input => +input;
 
-const fillBuffer = (iterations, steps) => {
+export const part1 = input => {
   const buffer = [0];
   let pos = 0;
 
-  for (let i = 0; i < iterations; i += 1) {
+  for (let i = 0; i < 2017; i += 1) {
     const bufferLength = i + 1;
-    pos += steps >= bufferLength ? steps % bufferLength : steps;
+    pos += input >= bufferLength ? input % bufferLength : input;
     pos = (pos >= bufferLength ? pos - bufferLength : pos) + 1;
     buffer.splice(pos, 0, i + 1);
   }
-  return [buffer, pos];
-};
-
-export const part1 = input => {
-  const [buffer, lastPosition] = fillBuffer(2017, input);
-  return buffer[lastPosition + 1];
+  return buffer[pos + 1];
 };
 
 export const part2 = input => {
-  const result = part1(input);
-  return result;
+  let firstPosValue;
+  let pos = 0;
+
+  for (let i = 0; i < 50000000; i += 1) {
+    const bufferLength = i + 1;
+    pos += input >= bufferLength ? input % bufferLength : input;
+    pos = (pos >= bufferLength ? pos - bufferLength : pos) + 1;
+    if (pos === 1) {
+      firstPosValue = i + 1;
+    }
+  }
+  return firstPosValue;
 };
