984bd8001fd39d7ada20183fd7072021de85d1c4
Borys Ponomarenko <bponomarenko@gmail.com>
2022-12-15 15:30:24 +0100

    Greatly simplify the solution

--- puzzles/2022/day14/solution.js
+++ puzzles/2022/day14/solution.js
@@ -2,87 +2,77 @@ export const formatInput = input => input.split('\n').map(line => line.split(' -
 
 const findRocks = lines => {
   const rocks = new Set();
+  let leftX = Infinity;
+  let rightX = -Infinity;
+  let bottomY = -Infinity;
+
   lines.forEach(line => {
     for (let i = 1; i < line.length; i += 1) {
       const [xs, ys] = line[i - 1];
       const [xe, ye] = line[i];
+      leftX = Math.min(leftX, xs, xe);
+      rightX = Math.max(rightX, xs, xe);
+      bottomY = Math.max(bottomY, ys + 2, ye + 2);
 
-      // move by Y
       if (xs === xe) {
+        // move by Y
         for (let dy = 0, my = Math.abs(ys - ye); dy <= my; dy += 1) {
           rocks.add(`${xs},${ys < ye ? ys + dy : ys - dy}`);
         }
-      }
-
-      // move by X
-      if (ys === ye) {
+      } else if (ys === ye) {
+        // move by X
         for (let dx = 0, mx = Math.abs(xs - xe); dx <= mx; dx += 1) {
           rocks.add(`${xs < xe ? xs + dx : xs - dx},${ys}`);
         }
       }
     }
   });
-  return rocks;
+  return { rocks, leftX, rightX, bottomY };
 };
 
-const findLimits = rocks => {
-  const arr = Array.from(rocks).map(coord => coord.split(',').map(Number));
-  const xx = arr.map(([x]) => x);
-  return [Math.min(...xx), Math.max(...xx), Math.max(...arr.map(([, y]) => y)) + 2];
-};
+const findStartY = rocks => Array.from(rocks)
+  .map(line => (line.startsWith('500,') ? +line.split(',')[1] : null))
+  .filter(Boolean)
+  .sort((a, b) => a - b)[0] - 1;
 
 const fillSand = (input, checkLimits = true) => {
-  const rocks = findRocks(input);
-  const [lx, rx, bottom] = findLimits(rocks);
-  const filled = new Set(rocks);
-  const initialSize = filled.size;
+  const { rocks, leftX, rightX, bottomY } = findRocks(input);
+  const start = [500, findStartY(rocks)];
 
-  const isFree = (x, y) => (checkLimits ? true : y !== bottom) && !filled.has(`${x},${y}`);
-  const isInbound = (x, y) => (checkLimits ? x >= lx && x <= rx : true) && y < bottom;
-  const isFreeAndInbound = (x, y) => isFree(x, y) && isInbound(x, y);
-  const sinkDown = (x, y) => {
-    while (isFreeAndInbound(x, y + 1)) {
-      y += 1;
-    }
-    return y;
-  };
+  const isInbound = (x, y) => y > 0 && (checkLimits ? x >= leftX && x <= rightX && y < (bottomY - 2) : true);
+  const isFree = (x, y) => y !== bottomY && !rocks.has(`${x},${y}`);
+  let count = checkLimits ? 0 : 1;
 
-  let x;
-  let y;
-
-  do {
-    // Find first position for attempt
-    x = 500;
-    y = sinkDown(x, 0);
-
-    do {
-      // attempt to roll to the left
-      if (isFree(x - 1, y + 1)) {
+  for (;;) {
+    let [x, y] = start;
+    for (;;) {
+      if (isFree(x, y + 1)) {
+        // down
+        y += 1;
+      } else if (isFree(x - 1, y + 1)) {
+        // left
         x -= 1;
-        y = sinkDown(x, y + 1);
-        // attempt to find another position
-        continue;
-      }
-
-      // attempt to roll to the right
-      if (isFree(x + 1, y + 1)) {
+        y += 1;
+      } else if (isFree(x + 1, y + 1)) {
+        // right
         x += 1;
-        y = sinkDown(x, y + 1);
-        // attempt to find another position
-        continue;
+        y += 1;
+      } else {
+        // can't move anywhere else
+        break;
       }
-      // Settle at the current place
-      filled.add(`${x},${y}`);
-    } while (isFreeAndInbound(x, y));
-
-    // Check the bounds
-    if (!isInbound(x, y)) {
-      // last entry is in a free fall or on the floor – remove it
-      filled.delete(`${x},${y}`);
-      break;
     }
-  } while (y > 0);
-  return filled.size - initialSize;
+
+    if (isInbound(x, y)) {
+      rocks.add(`${x},${y}`);
+      count += 1;
+      if (x === 500) {
+        start[1] = Math.min(y - 1, start[1]);
+      }
+    } else {
+      return count;
+    }
+  }
 };
 
 export const part1 = input => fillSand(input);
