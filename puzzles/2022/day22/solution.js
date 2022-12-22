10a7d1cc78f0635c495aa1be6ec3e869d753d2a6
Borys Ponomarenko <bponomarenko@gmail.com>
2022-12-22 21:43:38 +0100

    Add some tests for the part 2

--- puzzles/2022/day22/solution.js
+++ puzzles/2022/day22/solution.js
@@ -53,62 +53,63 @@ export const part1 = ([grid, path]) => followThePath(grid, path, (x, y, dir) => 
   return null;
 });
 
-const size = 50;
-
-export const part2 = ([grid, path]) => followThePath(grid, path, (x, y, dir) => {
-  // Do woodoo "wrapping" around the sides of the cube
-  // (solution hardcoded to the data structure of the input file, so it doesn't work for the example from the puzzle)
-  switch (dir) {
-    case 'r':
-      if (x + 1 >= grid[y].length) {
-        if (y < size) {
-          return [[size * 2 - 1, size * 3 - 1 - y], 'l'];
+export const part2 = ([grid, path], isTest) => {
+  const size = isTest ? 4 : 50;
+  return followThePath(grid, path, (x, y, dir) => {
+    // Do woodoo "wrapping" around the sides of the cube
+    // (solution hardcoded to the data structure of the input file, so it doesn't work for the example from the puzzle)
+    switch (dir) {
+      case 'r':
+        if (x + 1 >= grid[y].length) {
+          if (y < size) {
+            return [[size * 2 - 1, size * 3 - 1 - y], 'l'];
+          }
+          if (y < size * 2) {
+            return [[y + size, size - 1], 'u'];
+          }
+          if (y < size * 3) {
+            return [[size * 3 - 1, size * 3 - 1 - y], 'l'];
+          }
+          return [[y - size * 2, size * 3 - 1], 'u'];
         }
-        if (y < size * 2) {
-          return [[y + size, size - 1], 'u'];
+        return [[x + 1, y], dir];
+      case 'l':
+        if (x - 1 < 0 || grid[y][x - 1] === ' ') {
+          if (y < size) {
+            return [[0, size * 3 - 1 - y], 'r'];
+          }
+          if (y < size * 2) {
+            return [[y - size, size * 2], 'd'];
+          }
+          if (y < size * 3) {
+            return [[size, size * 3 - 1 - y], 'r'];
+          }
+          return [[y - size * 2, 0], 'd'];
         }
-        if (y < size * 3) {
-          return [[size * 3 - 1, size * 3 - 1 - y], 'l'];
+        return [[x - 1, y], dir];
+      case 'd':
+        if (y + 1 >= grid.length || x >= grid[y + 1].length) {
+          if (x < size) {
+            return [[x + size * 2, 0], dir];
+          }
+          if (x < size * 2) {
+            return [[size - 1, size * 2 + x], 'l'];
+          }
+          return [[size * 2 - 1, x - size], 'l'];
         }
-        return [[y - size * 2, size * 3 - 1], 'u'];
-      }
-      return [[x + 1, y], dir];
-    case 'l':
-      if (x - 1 < 0 || grid[y][x - 1] === ' ') {
-        if (y < size) {
-          return [[0, size * 3 - 1 - y], 'r'];
+        return [[x, y + 1], dir];
+      case 'u':
+        if (y - 1 < 0 || grid[y - 1][x] === ' ') {
+          if (x < size) {
+            return [[size, x + size], 'r'];
+          }
+          if (x < size * 2) {
+            return [[0, x + size * 2], 'r'];
+          }
+          return [[x - size * 2, size * 4 - 1], dir];
         }
-        if (y < size * 2) {
-          return [[y - size, size * 2], 'd'];
-        }
-        if (y < size * 3) {
-          return [[size, size * 3 - 1 - y], 'r'];
-        }
-        return [[y - size * 2, 0], 'd'];
-      }
-      return [[x - 1, y], dir];
-    case 'd':
-      if (y + 1 >= grid.length || x >= grid[y + 1].length) {
-        if (x < size) {
-          return [[x + size * 2, 0], dir];
-        }
-        if (x < size * 2) {
-          return [[size - 1, size * 2 + x], 'l'];
-        }
-        return [[size * 2 - 1, x - size], 'l'];
-      }
-      return [[x, y + 1], dir];
-    case 'u':
-      if (y - 1 < 0 || grid[y - 1][x] === ' ') {
-        if (x < size) {
-          return [[size, x + size], 'r'];
-        }
-        if (x < size * 2) {
-          return [[0, x + size * 2], 'r'];
-        }
-        return [[x - size * 2, size * 4 - 1], dir];
-      }
-      return [[x, y - 1], dir];
-  }
-  return null;
-});
+        return [[x, y - 1], dir];
+    }
+    return null;
+  });
+};
