2751e6b03fc93cdd8d1f314f7095e45ac92ad360
Borys Ponomarenko <bponomarenko@gmail.com>
2023-12-03 13:16:08 +0100

    2d array, generators, regexes... and it is only day 3 o_O

---
+++ puzzles/2023/day03/solution.js
@@ -0,0 +1,49 @@
+import { getAdjacent } from '../../utils/grid.js';
+import { sum, power } from '../../utils/collections.js';
+
+export const formatInput = input => input.split('\n');
+
+const partRe = /[^\d.]/;
+const digitRe = /\d/;
+
+const collectNum = (schema, x, y, part) => {
+  const line = schema[y].replaceAll(part, '.');
+  const startIndex = line.lastIndexOf('.', x);
+  const endIndex = line.indexOf('.', x);
+  const limits = [startIndex === -1 ? 0 : startIndex + 1, endIndex === -1 ? line.length : endIndex];
+  return [+line.substring(limits[0], limits[1]), ...limits];
+};
+
+function* parts(schema, partType) {
+  const collected = new Set();
+
+  for (let y = 0, maxY = schema.length; y < maxY; y += 1) {
+    const line = schema[y];
+    for (let x = 0, maxX = line.length; x < maxX; x += 1) {
+      const part = line[x];
+      if (partType ? part === partType : partRe.test(part)) {
+        const partNums = getAdjacent(x, y, [0, maxX - 1], [0, maxY - 1])
+          .map(([ax, ay]) => {
+            if (digitRe.test(schema[ay][ax]) && !collected.has(`${ax},${ay}`)) {
+              const [num, minX, minY] = collectNum(schema, ax, ay, part);
+              for (let dx = minX; dx < minY; dx += 1) {
+                collected.add(`${dx},${ay}`);
+              }
+              return num;
+            }
+            return null;
+          })
+          .filter(Boolean);
+
+        yield partNums;
+      }
+    }
+  }
+}
+
+export const part1 = input => sum(Array.from(parts(input)).flat());
+
+export const part2 = input => {
+  const nums = Array.from(parts(input, '*')).map(partNums => (partNums.length === 2 ? power(partNums) : 0));
+  return sum(nums);
+};
