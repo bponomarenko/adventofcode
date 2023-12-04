90f5dcd1b8c27b712e2fb405891cd0ccda504fa7
Borys Ponomarenko <bponomarenko@gmail.com>
2023-12-04 07:57:19 +0100

    I'm sure this can be rewritten in a smarter way, but it gave me the answer :)

---
+++ puzzles/2023/day04/solution.js
@@ -0,0 +1,21 @@
+import { sum } from '../../utils/collections.js';
+
+export const formatInput = input => input.split('\n').map(line => {
+  const cardNums = line.split(': ')[1];
+  const [winning, nums] = cardNums.split(' | ').map(group => group.split(' ').filter(Boolean).map(Number));
+  const win = nums.filter(num => winning.includes(num));
+  return win.length > 0 ? [2 ** (win.length - 1), win.length, 1] : [0, 0, 1];
+});
+
+export const part1 = input => sum(input.map(([score]) => score));
+
+export const part2 = input => {
+  input.forEach(([, count, pile], index) => {
+    if (count > 0) {
+      for (let i = index + 1; i <= index + count; i += 1) {
+        input[i][2] += pile;
+      }
+    }
+  });
+  return sum(input.map(([, , pile]) => pile));
+};
