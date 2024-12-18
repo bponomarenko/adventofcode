3cd1d21572248059ca40548da297a2b6e1e3ed93
Borys Ponomarenko <bponomarenko@gmail.com>
2024-12-18 06:43:23 +0100

    "falling bytes"... that is something new

---
+++ puzzles/2024/day18/solution.js
@@ -0,0 +1,78 @@
+import BinaryHeap from '../../utils/binary-heap.js';
+import { getStraightAdjacent } from '../../utils/grid.js';
+
+export const formatInput = input => {
+  const bytes = input.split('\n').map(row => row.split(',').map(Number));
+  const size = Math.max(...bytes.map(([x]) => x)) + 1;
+  return [bytes, Array.from(new Array(size), () => new Array(size).fill('.'))];
+};
+
+const findShortestPath = (grid, [sx, sy], [ex, ey]) => {
+  const limits = grid.gridLimits();
+  const visited = new Set();
+  const queue = new BinaryHeap(state => state.length, state => state.hash);
+  queue.push({ x: sx, y: sy, hash: `${sx},${sy}`, path: new Set([`${sx},${sy}`]) });
+
+  while (queue.size) {
+    // 1. Get next state
+    const state = queue.pop();
+
+    // 2. Check if we got to the exit
+    if (state.x === ex && state.y === ey) {
+      return state.path;
+    }
+
+    // 3. Mark it as visited
+    visited.add(state.hash);
+
+    // 3. Find next states
+    getStraightAdjacent(state.x, state.y, ...limits).forEach(([x, y]) => {
+      const hash = `${x},${y}`;
+      if (visited.has(hash) || grid[y][x] === '#') {
+        return;
+      }
+      const path = new Set(state.path);
+      path.add(hash);
+
+      if (queue.has({ hash })) {
+        const queuedItem = queue.get({ hash });
+        if (path.size < queuedItem.path.size) {
+          queuedItem.path = path;
+          queue.reposition(queuedItem);
+        }
+      } else {
+        queue.push({ x, y, path, hash });
+      }
+    });
+  }
+  return null;
+};
+
+export const part1 = ([bytes, grid], isTest) => {
+  bytes.slice(0, isTest ? 12 : 1024).forEach(([x, y]) => {
+    grid[y][x] = '#';
+  });
+  const max = grid.length - 1;
+  return findShortestPath(grid, [0, 0], [max, max]).size - 1;
+};
+
+export const part2 = ([bytes, grid]) => {
+  const max = grid.length - 1;
+  const params = [[0, 0], [max, max]];
+  let path;
+  while (bytes.length) {
+    const [x, y] = bytes.shift();
+    grid[y][x] = '#';
+    const hash = `${x},${y}`;
+    if (path?.has(hash)) {
+      path = null;
+    }
+    if (!path) {
+      path = findShortestPath(grid, ...params);
+    }
+    if (!path) {
+      return hash;
+    }
+  }
+  return null;
+};
