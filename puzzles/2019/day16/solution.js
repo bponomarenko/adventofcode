56e5586eef45047e5cfb4e850377392d406394ab
Borys Ponomarenko <bponomarenko@gmail.com>
2024-12-19 06:38:40 +0100

    Restructure for part2

--- puzzles/2019/day16/solution.js
+++ puzzles/2019/day16/solution.js
@@ -1,32 +1,38 @@
-export const formatInput = input => input.split('').map(Number);
+export const formatInput = input => input;
 
-const applyFFT = (input, phases) => {
-  const len = input.length;
+const phaseSignal = signal => {
+  const len = signal.length;
+  let phasedSignal = '';
+  for (let i = 0; i < len; i += 1) {
+    let sum = 0;
+    for (let p = i; p < len; p += 4 * (i + 1)) {
+      for (let d = 0; d < i + 1 && p + d < len; d += 1) {
+        sum += +signal[p + d];
+      }
+    }
+    for (let p = 3 * i + 2; p < len; p += 4 * (i + 1)) {
+      for (let d = 0; d < i + 1 && p + d < len; d += 1) {
+        sum -= signal[p + d];
+      }
+    }
+    phasedSignal += Math.abs(sum % 10);
+  }
+  return phasedSignal;
+};
+
+const applyFFT = (signal, offset = 0) => {
+  let phases = 100;
   while (phases) {
-    input = input.map((_, i) => {
-      let sum = 0;
-      for (let p = i; p < len; p += 4 * (i + 1)) {
-        for (let d = 0; d < i + 1 && p + d < len; d += 1) {
-          sum += input[p + d];
-        }
-      }
-      for (let p = 3 * i + 2; p < len; p += 4 * (i + 1)) {
-        for (let d = 0; d < i + 1 && p + d < len; d += 1) {
-          sum -= input[p + d];
-        }
-      }
-      return Math.abs(sum % 10);
-    });
+    signal = phaseSignal(signal);
     phases -= 1;
   }
-  return input;
+  return signal.slice(offset, offset + 8);
 };
 
-export const part1 = input => applyFFT(input, 100).slice(0, 8).join('');
+export const part1 = signal => applyFFT(signal);
 
-export const part2 = input => {
-  // It should work, but of course Node.js can't handle it. Should come up with different approach to solve it
-  const offset = +input.slice(0, 7).join('');
-  const message = applyFFT(input.join('').repeat(10000).split('').map(Number), 100);
-  return message.slice(offset, offset + 8).join('');
-};
+// It should work, but of course Node.js can't handle it. Should come up with different approach to solve it
+export const part2 = signal => applyFFT(
+  new Array(10000).fill(signal).join(''),
+  +signal.slice(0, 7),
+);
