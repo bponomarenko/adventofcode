a036cde57c888aabed3d5cf8d09cae6ef0d945ff
Borys Ponomarenko <bponomarenko@gmail.com>
2024-12-22 13:52:32 +0100

    Figured how not to avoid BigInt, but also edge case where same sequence could result in different price

--- puzzles/2024/day22/solution.js
+++ puzzles/2024/day22/solution.js
@@ -1,67 +1,38 @@
-export const formatInput = input => input.split('\n').map(BigInt);
+export const formatInput = input => input.split('\n').map(Number);
 
-const mix = (secret, num) => secret ^ num % BigInt(16777216);
+const mix = (secret, num) => secret ^ num & 0xFFFFFF;
 
-const evolve = (secret, count = 2000) => {
+const evolve = secret => {
   const changes = [];
+  let count = 2000;
   while (count > 0) {
-    const lastBit = secret % BigInt(10);
-    secret = mix(secret, secret * BigInt(64));
-    secret = mix(secret, secret / BigInt(32));
-    secret = mix(secret, secret * BigInt(2048));
-    const newBit = secret % BigInt(10);
+    const lastBit = secret % 10;
+    secret = mix(secret, secret << 6);
+    secret = mix(secret, secret >> 5);
+    secret = mix(secret, secret << 11);
+    const newBit = secret % 10;
     changes.push([newBit, newBit - lastBit]);
     count -= 1;
   }
   return [secret, changes];
 };
 
-const sum = (arr, fn) => arr.reduce((acc, item, i) => acc + fn(item, i), BigInt(0));
-
-export const part1 = input => sum(input, secret => evolve(secret)[0]).toString();
-
-function* changeDiffs(from, count) {
-  for (let i = 9; i >= 0; i -= 1) {
-    const diff = i - from;
-    if (count === 0) {
-      yield diff;
-    } else {
-      for (const change of changeDiffs(i, count - 1)) {
-        yield `${diff},${change}`;
-      }
-    }
-  }
-}
-
-function* changeSequences() {
-  const memory = new Set();
-  for (let i = 9; i >= 0; i -= 1) {
-    for (const change of changeDiffs(i, 3)) {
-      if (memory.has(change)) {
-        continue;
-      }
-      memory.add(change);
-      yield change;
-    }
-  }
-}
+export const part1 = input => input.sum(secret => evolve(secret)[0]);
 
 export const part2 = input => {
+  const allSequences = new Set();
   const sequences = input.map(secret => {
     const changes = evolve(secret)[1];
     const memory = new Map();
     for (let i = 3; i < changes.length; i += 1) {
-      memory.set(`${changes[i - 3][1]},${changes[i - 2][1]},${changes[i - 1][1]},${changes[i][1]}`, changes[i][0]);
+      const sequence = `${changes[i - 3][1]},${changes[i - 2][1]},${changes[i - 1][1]},${changes[i][1]}`;
+      if (memory.has(sequence)) {
+        continue;
+      }
+      memory.set(sequence, changes[i][0]);
+      allSequences.add(sequence);
     }
     return memory;
   });
-
-  let max = BigInt(0);
-  for (const sequence of changeSequences()) {
-    const total = sum(sequences, memory => (memory.has(sequence) ? memory.get(sequence) : BigInt(0)));
-    if (total > max) {
-      max = total;
-    }
-  }
-  return max.toString();
+  return Math.max(...Array.from(allSequences).map(sequence => sequences.sum(memory => (memory.has(sequence) ? memory.get(sequence) : 0))));
 };
