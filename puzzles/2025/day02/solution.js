e46351a4d877bf1d75e551211de497afc49c0513
Borys Ponomarenko <bponomarenko@gmail.com>
2025-12-03 14:19:29 +0000

    Simplify a bit a solution for day 2

--- puzzles/2025/day02/solution.js
+++ puzzles/2025/day02/solution.js
@@ -1,15 +1,18 @@
 export const formatInput = input => input.split(',').map(range => range.split('-').map(Number));
 
-const filterInvalidIDs = (input, reg) => input.flatMap(([start, end]) => {
-  const res = [];
-  for (let i = start; i <= end; i += 1) {
-    if (reg.test(i)) {
-      res.push(i);
+const filterInvalidIDs = (input, multiple = false) => {
+  const reg = new RegExp(`^(.+)\\1${multiple ? '+' : ''}$`);
+  return input.flatMap(([start, end]) => {
+    const res = [];
+    for (let i = start; i <= end; i += 1) {
+      if (reg.test(i)) {
+        res.push(i);
+      }
     }
-  }
-  return res;
-}).sum();
+    return res;
+  }).sum();
+};
 
-export const part1 = input => filterInvalidIDs(input, /^(.+)\1$/);
+export const part1 = input => filterInvalidIDs(input);
 
-export const part2 = input => filterInvalidIDs(input, /^(.+)\1{1,}$/);
+export const part2 = input => filterInvalidIDs(input, true);
