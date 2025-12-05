36dcd067a17a4f0524b75c61e82829b16e60bc26
Borys Ponomarenko <bponomarenko@gmail.com>
2025-12-05 07:23:56 +0100

    Day 5 is done

---
+++ puzzles/2025/day05/solution.js
@@ -0,0 +1,30 @@
+export const formatInput = input => {
+  const [ranges, ids] = input.split('\n\n');
+  return [
+    ranges.split('\n').map(line => line.split('-').map(Number)),
+    ids.split('\n').map(Number),
+  ];
+};
+
+export const part1 = ([ranges, ids]) => ids.filter(id => ranges.some(([min, max]) => id >= min && id <= max)).length;
+
+export const part2 = ([ranges]) => {
+  let merged;
+  do {
+    merged = false;
+    const newRanges = [];
+    // Find all the overlapping ranges and merge them
+    ranges.forEach(([min1, max1]) => {
+      const index = newRanges.findIndex(([min2, max2]) => min2 <= max1 && max2 >= min1);
+      if (index === -1) {
+        newRanges.push([min1, max1]);
+      } else {
+        const [min2, max2] = newRanges[index];
+        newRanges[index] = [Math.min(min2, min1), Math.max(max2, max1)];
+        merged = true;
+      }
+    });
+    ranges = newRanges;
+  } while (merged);
+  return ranges.map(([min, max]) => max - min + 1).sum();
+};
